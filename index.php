141c15f743aca119b21d26247f9f2d12e73791a7
AnnKemp <ann.kemp@scarlet.be>
2020-02-11 11:12:31 +0100

    form-part adress works + check errors works too

--- index.php
+++ index.php
@@ -4,42 +4,74 @@ session_start();
 $totalValue = 0;
 $_SESSION["total"]=$totalValue;  // om waarden in de sessies op te slaan
 
-//ini_set('display_errors', "1"); om foutmeldingen te tonen
-//include 'validation.php';
+ini_set('display_errors', "1"); // om foutmeldingen te tonen
 
-function whatIsHappening() {
-    echo '<h2>$_GET</h2>';
-    var_dump($_GET);
-    echo '<h2>$_POST</h2>';
-    var_dump($_POST);
-    echo '<h2>$_COOKIE</h2>';
-    var_dump($_COOKIE);
-    echo '<h2>$_SESSION</h2>';
-    var_dump($_SESSION);
-}
 // define variables and set to empty values
+
+// for the adress
 $email = $street = $streetnr = $city = $zipcode = "";
+// for the error messages
+$emailErr = $streetErr = $streetnrErr = $cityErr = $zipcodeErr = "";
 // de postwaarden uit het formulier halen
 
-//if (!empty($_POST)) { // indien het formulier gesubmit wordt
+//if (!empty($_POST)) { // if submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
-   
-$email=test_input($_POST["email"]);  // straat ophalen
-   
-//adres
-$street=test_input($_POST["street"]);  // straat ophalen
-$streetnr=test_input($_POST["streetnumber"]); // nr ophalen
-$city=test_input($_POST["city"]); // dorp/gemeente/stad ophalen
-$zipcode=test_input($_POST["zipcode"]); // postnummer ophalen 
-    
-function test_input(str $data) // nog eens checken of die str juist is voor strict typing
-{
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  // functie op validation page aanroepen voor validatie?
-  return (string) $data;
-}
+
+    function test_input(string $data) // trimmen, stripslashes and specialchars
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        // functie op validation page aanroepen voor validatie?
+        return (string) $data;
+    }
+
+    if (empty($_POST["email"])) {
+        $emailErr = "* Email is required";
+    } else {
+
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) { // deze filter werkt niet waterdicht, beter zelf schrijven maar voorlopig bij gebrek aan tijd
+            $email=test_input($_POST["email"]);
+        }else{
+            $emailErr = "* Invalid email format!";
+        }
+    }
+
+    if (empty($_POST["street"])) {
+        $streetErr = "* Street is required";
+    } else {
+        $street=test_input($_POST["street"]);  // straat ophalen
+    }
+
+    if (empty($_POST["streetnumber"])) {
+        $streetnrErr = "* Streetnumber is required";
+
+    } else {
+        $streetnr=test_input($_POST["streetnumber"]); // nr ophalen
+        if(is_numeric($streetnr)){                      // test if number
+            $streetnr=test_input($_POST["streetnumber"]); // nr ophalen
+        }else{
+            $streetnrErr = "* It has to be a number!";
+        }
+    }
+
+    if (empty($_POST["city"])) {
+        $cityErr = "* City is required";
+    } else {
+        $city=test_input($_POST["city"]); // dorp/gemeente/stad ophalen
+    }
+
+    if (empty($_POST["zipcode"])) {
+        $zipcodeErr = "* Zipcode is required";
+    } else {
+        $zipcode=test_input($_POST["zipcode"]); // postnummer ophalen
+        if(is_numeric($zipcode)){               // test if number
+            $zipcode=test_input($_POST["zipcode"]); // postnummer ophalen
+        }else{
+            $zipcodeErr = "* It has to be a number!";
+        }
+    }
+
    // date_default_timezone_set("America/New_York");
 //echo "Today is " . date("d-m-Y") . "<br>"; echo "The time is " . date("H:i:s");
     
@@ -58,9 +90,8 @@ mail(COOK,$subject,$bestelling,$headers);
 
 // via de get-waarden de juiste producten selecteren food of drink
 if (!empty($_GET)) {
-
 $foodnr=$_GET['food'];
-echo $foodnr;    
+
     
 if($foodnr===1){    
    // nog wat code schrijven om de producten te tonen 
@@ -79,7 +110,6 @@ $products = [
     ['name' => 'Club Chicken', 'price' => 4],
     ['name' => 'Club Salmon', 'price' => 5]
 ];
-    
 }
  if($foodnr===0){ 
      // nog wat code schrijven om de producten te tonen 
@@ -89,17 +119,13 @@ $products = [
     ['name' => 'Sprite', 'price' => 2],
     ['name' => 'Ice-tea', 'price' => 3],
 ];
-    
  }
 }else{
-    echo "geen GET ontvangen!";
+    echo "Please, choose something to eat or drink!";
 }
-
 // code schrijven om de geselecteerde waarden op te tellen en in de session te steken
     // to change a session variable, just overwrite it
 //$_SESSION["favcolor"] = "yellow";
 //print_r($_SESSION);
-
-
 require 'form.php';
 ?>
