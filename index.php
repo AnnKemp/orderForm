a993632d9134161c3e9c63d4e43907f60b645bed
AnnKemp <ann.kemp@scarlet.be>
2020-02-12 18:04:26 +0100

    last changes for today

--- index.php
+++ index.php
@@ -4,53 +4,40 @@ ini_set('display_errors', "1"); // om foutmeldingen te tonen
 session_start();
 
 // initiate variables
-$totalValue = 0;
+$totalValue=0;
 $cookie_email="";
-$cookie_total=0;
+$cookie_total="";
 $products =[];
-
-// start/setup the cookies for the total amount calculation  // dit moet ik nog uittesten
-setcookie($cookie_email, $cookie_total, time() + (86400 * 30), "/"); // 86400 = 1 day * 30 is for a month
+$foodDrink="";
+$price=0;
 
 // initiate variables for the adress
 $email = $street = $streetnr = $city = $zipcode = $consumation = "";
 // initiate variables for the error messages
 $emailErr = $streetErr = $streetnrErr = $cityErr = $zipcodeErr = "";
 
-
 //if (!empty($_POST)) { // if submitted  for the adress
 if ($_SERVER["REQUEST_METHOD"] == "POST"){  // --------start post data ---------------------------------------------------------------------------------------------
 
-    function test_input(string $data) // trimmen, stripslashes and specialchars
+    function test_input(string $data) // trimmen, stripslashes and specialchars  // is this old fashioned?
     {
         $data = trim($data);
         $data = stripslashes($data);
         $data = htmlspecialchars($data);
-        // functie op validation page aanroepen voor validatie?
         return (string) $data;
     }
+
     if (empty($_POST["email"])) {
         $emailErr = "* Email is required";
     } else {
         $email=test_input($_POST["email"]);
         if (filter_var($email, FILTER_VALIDATE_EMAIL)) { // deze filter werkt niet waterdicht, beter zelf schrijven maar voorlopig bij gebrek aan tijd . . .
             $email=test_input($_POST["email"]);
-
-            $cookie_email=$email;
-            $cookie_total=$totalValue;
-//$_COOKIE["total"]=$totalValue;  // om totaal bestelling in cookies op te slaan ook email en die checken om user te identificieren
-//$_COOKIE["email"]; // bepaalde tijd instellen voor die cookies en die anders maken
-
-            // to save the data for a longer term then a session, and to id the user you need the email
-           // $_COOKIE["email"]="$email";  // nog eens checken voor de exacte schrijfwijze
-
-            // to save the data per session
                $_SESSION["email"]=$email;
         }else{
             $emailErr = "* Invalid email format!";
         }
     }
-
     if (empty($_POST["street"])) {
         $streetErr = "* Street is required";
     } else {
@@ -70,7 +57,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){  // --------start post data ---------
             $streetnrErr = "* It has to be a number!";
         }
     }
-
     if (empty($_POST["city"])) {
         $cityErr = "* City is required";
     } else {
@@ -98,15 +84,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){  // --------start post data ---------
             // var_dump($contents);
 
             foreach ($contents as $x => $x_value) {
-               // echo "Product: " . $x . ", Price: " . $x_value;  // to show/test what's in it
-               // echo "<br>";
+                // echo "Product: " . $x . ", Price: " . $x_value;  // to show/test what's in it
+                // echo "<br>";
                 $foodDrink = $x;
-                $price = $x_value;
+                $price = $price + $x_value;
                 // nog een session van maken om deze ook naar een andere pagina door te geven waar je de bestellingen toont
-               // $_SESSION["zipcode"] = $zipcode; how to set a session with two values? have to search it!
+                // $_SESSION["zipcode"] = $zipcode; how to set a session with two values? have to search it!
                 // maybe with the array as value? so $contents in a session so you need to do a foreach again to get to the values?
+                // to save the data for a longer term then a session, and to id the user you need the email
+                // set the expiration date to one hour ago to delete the cookie
+                //setcookie("user", "", time() - 3600);
+
+                // to save the data per session
             }
+            $price=strval($price);
+            echo $price;
         }
+            // find a way to set a cookie
+            setcookie("$cookie_email", "$price", time() + (86400 * 30)); // 86400 = 1 day * 30 is for a month
+            //setcookie("cookie_email", "$price", time() + (86400 * 30)); // 86400 = 1 day * 30 is for a month
+//$_COOKIE["email"]=$totalValue;  // om totaal bestelling in cookies op te slaan ook email en die checken om user te identificieren
+
+            if(!isset($_COOKIE[$cookie_email])) {
+                echo "Cookie named ".$cookie_email." is not set!";
+             } else {
+                echo "Cookie ".$cookie_email ." is set!<br>";
+                echo "Value is: " . $_COOKIE[$cookie_email];
+            }
+
 //------------------------------------------------------ calculate the delivery time after choosing the mode: normal or express ---------------------------------------------
             // the delivery choiche
             $delivery_time=0; // initialise vars
@@ -121,8 +126,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){  // --------start post data ---------
                $time=date("h");
                 // the time + two for the delivery-time
                $time=$time+2;
-                echo '<p class="textAbove">The delivery time is ' . date("$time:i")."</p>";
-                $delivery_time=date("$time:i");
+                echo '<p class="textAbove">Your order will be delivered at: ' . date("$time:ia")."</p>";
+                $delivery_time=date("$time:ia");
            }
             elseif (isset($delivery) && $delivery=="express"){ // if isset and they have choosen express delivery
                 //a delivery time of 45 minutes
@@ -137,7 +142,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){  // --------start post data ---------
                     $minutes=$minutes-60;
                     $hours=$hours+1;
                 }
-                echo '<p class="textAbove">The delivery time is ' . date("$hours:$minutes")."</p>";
+                echo '<p class="textAbove">Your order will be delivered at: ' . date("$hours:$minutes")."</p>";
 
                  $delivery_time=date("$hours:$minutes");
 
